7496d43d34421d8f2469af16835f5e0da99350c0
Anders Åslund <aad@learningwell.se>
2013-10-08 20:30:54 +0200

    Adding proxy support

--- lib/stddtool.rb
+++ lib/stddtool.rb
@@ -65,7 +65,7 @@ require 'ostruct'
     def postFeature(featureObj)
       uri = URI.parse(@url)
       proxy = ENV['http_proxy'] ? URI.parse(ENV['http_proxy']) : OpenStruct.new
-      http = Net::HTTP::Proxy(proxy.host, proxy.port).new(url.host, url.port)
+      http = Net::HTTP::Proxy(proxy.host, proxy.port).new(uri.host, uri.port)
       request = Net::HTTP::Post.new("/collectionapi/features")
       request.add_field('X-Auth-Token', '97f0ad9e24ca5e0408a269748d7fe0a0')
       request.body = featureObj.to_json
@@ -89,17 +89,19 @@ require 'ostruct'
 
     def postStep(scenarioID,stepObj)
       uri = URI.parse(@url)
+      proxy = ENV['http_proxy'] ? URI.parse(ENV['http_proxy']) : OpenStruct.new
       path = "/collectionapi/scenarios/#{scenarioID}"
       req = Net::HTTP::Put.new(path, initheader = { 'X-Auth-Token' => '97f0ad9e24ca5e0408a269748d7fe0a0'})
       req.body = stepObj.to_json
-      response = Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
+      response = Net::HTTP::Proxy(proxy.host, proxy.port).new(uri.host, uri.port).start {|http| http.request(req) }
       # puts response.body
 
     end
 
     def postScenario(scenarioObj)
       uri = URI.parse(@url)
-      http = Net::HTTP.new(uri.host, uri.port)
+      proxy = ENV['http_proxy'] ? URI.parse(ENV['http_proxy']) : OpenStruct.new
+      http = Net::HTTP::Proxy(proxy.host, proxy.port).new(uri.host, uri.port)
       request = Net::HTTP::Post.new("/collectionapi/scenarios")
       request.add_field('X-Auth-Token', '97f0ad9e24ca5e0408a269748d7fe0a0')
       request.body = scenarioObj.to_json
